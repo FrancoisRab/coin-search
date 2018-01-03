2655cc2f264718204803f6617de54fc602d9f5da
Francois <rabanel.francois@gmail.com>
2018-01-03 17:56:10 +0100

    new and create function add

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -4,19 +4,31 @@ class StoresController < ApplicationController
     @stores = Store.all
   end
 
+  def show
+    @store = Store.find(params[:id])
+  end
+
   def new
     @store = Store.new
   end
 
   def create
-  end
-
-  def edit
-  end
-
-  def update
+    @store = Store.new(store_params)
+    @store.user = current_user
+    if @store.save
+      redirect_to store_path(@store)
+    else
+      render :new
+    end
   end
 
   def destroy
   end
+
+  private
+
+  def store_params
+    params.require(:store).permit(:name, :category, :description, :address, :postcode)
+  end
+
 end
