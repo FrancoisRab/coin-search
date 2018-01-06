6dbba17aa7c97a84e0c1dd4162f034bbe41d1b91
Francois <rabanel.francois@gmail.com>
2018-01-06 18:14:56 +0100

    changement position scope profile

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,6 +1,6 @@
 class ProfilesController < ApplicationController
   def index
-    @stores = policy_scope(Store)
     @magasins = Store.where(user: current_user)
+    @stores = policy_scope(Store)
   end
 end
