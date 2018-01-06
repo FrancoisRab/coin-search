4d1a43742213ac8e145ed6068cf0066d2c9c5548
Francois <rabanel.francois@gmail.com>
2018-01-06 16:32:08 +0100

    pgsearch index ok

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,13 +2,13 @@ class SearchController < ApplicationController
   skip_before_action :authenticate_user!
 
   def index
-  @stores = policy_scope(Store).order(created_at: :desc)
-  if params[:query].present?
-    @search = Store.search(params[:query])
+  if params[:search].present?
+    @search = Store.search(params[:search])
     authorize @search
   else
     @search = Store.all
     authorize @search
   end
+  @stores = policy_scope(Store).order(created_at: :desc)
  end
 end
