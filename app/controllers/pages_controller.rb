fd827bf7047a287ebcbb8b78075868d99ae1c3af
Francois <rabanel.francois@gmail.com>
2018-01-08 10:45:32 +0100

    add geocoder pour prod

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,13 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!
 
   def home
+    @stores = Store.where.not(latitude: nil, longitude: nil)
+
+        @markers = @stores.map do |flat|
+          {
+            lat: flat.latitude,
+            lng: flat.longitude#,
+            # infoWindow: { content: render_to_string(partial: "/flats/map_box", locals: { flat: flat }) }
+          }
   end
 end
