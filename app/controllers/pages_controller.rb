3f3ac0318498f8b06701271d2632698997694ede
Francois <rabanel.francois@gmail.com>
2018-01-12 09:35:27 +0100

    fix footer bas de page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,14 +2,13 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!
 
   def home
-    @store = Store.all
     @stores = Store.where.not(latitude: nil, longitude: nil)
 
-        @markers = @stores.map do |flat|
+        @markers = @stores.map do |store|
           {
-            lat: flat.latitude,
-            lng: flat.longitude#,
-            # infoWindow: { content: render_to_string(partial: "/flats/map_box", locals: { flat: flat }) }
+            lat: store.latitude,
+            lng: store.longitude#,
+            # infoWindow: { content: render_to_string(partial: "/stores/map_box", locals: { store: store }) }
           }
         end
   end
--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,11 +4,17 @@ class SearchController < ApplicationController
   def index
   @stores = policy_scope(Store).order(created_at: :desc)
   if params[:query].present?
+    if params[:query].include? (0..300).to_s
+      @search = Store.near(params[:query],5)
+    end
     @search = Store.search(params[:query])
+    raise
     authorize @search
   else
     @search = Store.all
     authorize @search
   end
+
  end
 end
+
