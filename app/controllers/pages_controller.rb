48c8373260014ee247303f4e605a2f41b77ad613
Francois <rabanel.francois@gmail.com>
2018-01-14 14:13:47 +0100

    add last stores on homepage + wrapper content homepage

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!
 
   def home
+    @store_all = Store.all
     @stores = Store.where.not(latitude: nil, longitude: nil)
 
         @markers = @stores.map do |store|
