0b063703034a428b4766678643b2f822731dcb57
Francois <rabanel.francois@gmail.com>
2018-01-15 15:06:56 +0100

    delete proload app.html.erb

--- app/javascript/packs/map.js
+++ app/javascript/packs/map.js
@@ -1,11 +1,46 @@
 import GMaps from 'gmaps/gmaps.js';
 import { autocomplete } from '../components/autocomplete';
 
+// const mapElement = document.getElementById('map');
+// if (mapElement) { // don't try to build a map if there's no div#map to inject in
+//   const map = new GMaps({ el: '#map', lat: 0, lng: 0 });
+//   const markers = JSON.parse(mapElement.dataset.markers);
+//   map.addMarkers(markers);
+//   if (markers.length === 0) {
+//     map.setZoom(2);
+//   } else if (markers.length === 1) {
+//     map.setCenter(markers[0].lat, markers[0].lng);
+//     map.setZoom(14);
+//   } else {
+//     map.fitLatLngBounds(markers);
+//   }
+// }
+
+
+
 const mapElement = document.getElementById('map');
 if (mapElement) { // don't try to build a map if there's no div#map to inject in
-  const map = new GMaps({ el: '#map', lat: 0, lng: 0 });
+  const map = new google.maps.Map(document.getElementById('map'), {
+            center: {lat: -34.397, lng: 150.644},
+            zoom: 6
+          });
   const markers = JSON.parse(mapElement.dataset.markers);
+  const infoWindow = new google.maps.InfoWindow({map: map});
   map.addMarkers(markers);
+
+  navigator.geolocation.getCurrentPosition(function(position) {
+    var pos = {
+      lat: position.coords.latitude,
+      lng: position.coords.longitude
+    };
+
+    infoWindow.setPosition(pos);
+    infoWindow.setContent('Location found.');
+    map.setCenter(pos);
+  }, function() {
+    handleLocationError(true, infoWindow, map.getCenter());
+  });
+
   if (markers.length === 0) {
     map.setZoom(2);
   } else if (markers.length === 1) {
@@ -15,3 +50,27 @@ if (mapElement) { // don't try to build a map if there's no div#map to inject in
     map.fitLatLngBounds(markers);
   }
 }
+
+
+
+
+
+
+
+// if (navigator.geolocation) {
+//   navigator.geolocation.getCurrentPosition(function(position) {
+//     var pos = {
+//       lat: position.coords.latitude,
+//       lng: position.coords.longitude
+//     };
+
+//     infoWindow.setPosition(pos);
+//     infoWindow.setContent('Location found.');
+//     map.setCenter(pos);
+//   }, function() {
+//     handleLocationError(true, infoWindow, map.getCenter());
+//   });
+// } else {
+//   // Browser doesn't support Geolocation
+//   handleLocationError(false, infoWindow, map.getCenter());
+// }
